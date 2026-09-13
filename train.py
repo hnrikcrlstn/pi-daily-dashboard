2ec46e608d3032c8c3e7cf715f55791530856523
Henrik <henrik.carlstein@gmail.com>
2026-09-13 14:37:22 +0200

    Added train station name lookup

--- train.py
+++ train.py
@@ -7,18 +7,45 @@ import json
 import logging
 
 def main():
-    make_api_call(requests.Session())
+    fetch_train_announcements(requests.Session())
     parse_trains()
     #fetch_train_data_loop()
 
 def fetch_train_data_loop():
     SESSION = requests.Session()
     while True:
-        make_api_call(SESSION)
+        fetch_train_announcements(SESSION)
+        parse_trains()
         time.sleep(config.TRAIN_SLEEP_DURATION)
 
+def fetch_station_name(station_code):
+    xml_request = (
+        "<REQUEST>"
+        f"<LOGIN authenticationkey='{config.TRAFIKVERKET_API_KEY}'/>"
+        "<QUERY objecttype='TrainStation' schemaversion='1.0'>"
+        "<FILTER>"
+        f"<EQ name='LocationSignature' value='{station_code}'/>"
+        "</FILTER>"
+        "</QUERY>"
+        "</REQUEST>"
+    )
 
-def make_api_call(trafik_session):
+    time.sleep(1)
+    try:
+        station_data = requests.post(config.TRAFIKVERKET_BASE_URL, headers={'Content-Type': 'application/xml'}, data=xml_request, timeout=(config.TRAIN_SLEEP_DURATION / 2))
+        station_data.raise_for_status()
+
+        return {
+                "code": station_code,
+                "name": station_data.json()["RESPONSE"]["RESULT"][0]["TrainStation"][0]["AdvertisedLocationName"],
+                "short_name": station_data.json()["RESPONSE"]["RESULT"][0]["TrainStation"][0]["AdvertisedShortLocationName"]
+            }
+    except requests.exceptions.RequestException as e:
+        logging.error(f"fan: {e}")
+    except Exception:
+        logging.exception(Exception)
+
+def fetch_train_announcements(trafik_session):
     log_filename = f"logs/run_{datetime.now().strftime('%Y-%m-%d')}.log"
     os.makedirs('logs', exist_ok=True)
     logging.basicConfig(
@@ -32,16 +59,16 @@ def make_api_call(trafik_session):
     xml_request = (
         "<REQUEST>"
         f"<LOGIN authenticationkey='{config.TRAFIKVERKET_API_KEY}'/>"
-        "<QUERY objecttype='TrainAnnouncement' schemaversion='1.8' orderby='AdvertisedTimeAtLocation' limit='150'>"
+        f"<QUERY objecttype='TrainAnnouncement' schemaversion='1.8' orderby='AdvertisedTimeAtLocation' limit='{config.TRAIN_API_LIMIT}'>"
         "<FILTER>"
         "<AND>"
         "<EQ name='ActivityType' value='Avgang' />"
         "<EQ name='InformationOwner' value='SL' />"
-        "<EQ name='LocationSignature' value='Upv' />"
+        f"<EQ name='LocationSignature' value='{config.TRAIN_STATION_SIGNATURE}' />"
         "<EQ name='Advertised' value='true' />"
         "<AND>"
-        "<GT name='AdvertisedTimeAtLocation' value='$dateadd(-0:30:00)' />"
-        "<LT name='AdvertisedTimeAtLocation' value='$dateadd(08:00:00)' />"
+        f"<GT name='AdvertisedTimeAtLocation' value='$dateadd({config.TRAIN_TIME_LIMIT_BEFORE})' />"
+        f"<LT name='AdvertisedTimeAtLocation' value='$dateadd({config.TRAIN_TIME_LIMIT_AFTER})' />"
         "</AND>"
         "</AND>"
         "</FILTER>"
@@ -59,80 +86,92 @@ def make_api_call(trafik_session):
     try:
         train_data = trafik_session.post(config.TRAFIKVERKET_BASE_URL, headers={'Content-Type': 'application/xml'}, data=xml_request, timeout=(config.TRAIN_SLEEP_DURATION / 2))
         train_data.raise_for_status()
-        log_point = json.dumps({
-            "fetched_at": f"{datetime.now()}",
-            "data": train_data.json()
-        })
+
         with open(config.TRAIN_CACHE_FILENAME, 'w') as f:
-            json.dump(log_point, f)
+            json.dump({
+                "fetched_at": f"{datetime.now()}",
+                "data": train_data.json()
+            }, f)
     except requests.exceptions.RequestException as e:
         logging.error(f"fan: {e}")
     except Exception:
         logging.exception(Exception)
 
+def confirm_station_names(trains):
+    station_dict = {}
+    cached_station_names = {}
+    new_station_names = []
+    for train in trains:
+        station_dict[train["ToLocation"][0]["LocationName"]] = True
+
+    if os.path.isfile(config.TRAIN_STATION_FILENAME):
+        with open(config.TRAIN_STATION_FILENAME, 'r') as f:
+            cached_station_names = json.load(f)
+    else:
+        with open(config.TRAIN_STATION_FILENAME, 'w') as f:
+            json.dump({}, f)
+
+    for station in station_dict.keys():
+        if station not in cached_station_names:
+            new_station_names.append(station)
+
+    for new_station in new_station_names:
+        cached_station_names[new_station] = fetch_station_name(new_station)
+
+    if len(new_station_names):
+        with open(config.TRAIN_STATION_FILENAME, 'w') as f:
+            json.dump(cached_station_names, f)
+    
+    return cached_station_names
+
 def parse_trains():
     cached_response = {}
-    with open(config.TRAIN_CACHE_FILENAME, 'r') as f:
-        cached_response = json.loads(json.load(f))
-    # TODO: remove the [0:10] limit
-    trains = cached_response['data']['RESPONSE']['RESULT'][0]['TrainAnnouncement'][0:30]
     parsed_trains = []
     current_messages = []
-    future_relevant_trains = {
-        'south': 0,
-        'north': 0
-    }
 
-    enough_trains_info_collected = False
+    with open(config.TRAIN_CACHE_FILENAME, 'r') as f:
+        cached_response = json.load(f)
+    if cached_response == {}:
+        logging.error("No cached response found, exiting loop")
+    trains = cached_response['data']['RESPONSE']['RESULT'][0]['TrainAnnouncement']
+
+    station_names = confirm_station_names(trains)
+
     for train in trains:
         arrival_time = datetime.fromisoformat(str(train["AdvertisedTimeAtLocation"]))
         now = datetime.now(arrival_time.tzinfo)
         time_delta = arrival_time - now
         northbound_train = train['ToLocation'][0]['LocationName'] in config.NORTHBOUND_STATIONS
-        
-        # Only collect arrival time info for future trains
-        if time_delta.total_seconds() > 0:
-            save_current_train = False
-            save
-            
 
-            if not enough_trains_info_collected:
-                enough_trains_info_collected = count_relevant_train_data(future_relevant_trains, 'B')
-                print(enough_trains_info_collected)
-                if northbound_train:
-                    if not count_relevant_train_data(future_relevant_trains, 'N') and train["ToLocation"][0]["LocationName"] == "U":
-                        save_current_train = True
-                        future_relevant_trains['north'] += 1
-                else:
-                    if not count_relevant_train_data(future_relevant_trains, 'S'):
-                        save_current_train = True
-                        future_relevant_trains['south'] += 1
-            
-            if save_current_train:
-                deviation = None
-                if "Deviation" in train and train["Deviation"][0]["Description"] not in config.UNIMPORTANT_MESSAGES:
-                    deviation = str(train["Deviation"][0]["Description"])
-                parsed_trains.append(
-                    {
-                        "northbound": northbound_train,
-                        "arrival_date": train["AdvertisedTimeAtLocation"].split('T')[0],
-                        "time_delta": time_delta.total_seconds(),
-                        "arrival_time": datetime.time(arrival_time),
-                        "canceled": train["Canceled"],
-                        "deviation": deviation
-                    }
-                )
-    print(parsed_trains)
+        current_deviations = []
+        short_train = False
+        if "Deviation" in train:
+            for deviation in train["Deviation"]:
+                if deviation["Description"] not in config.UNIMPORTANT_MESSAGES:
+                    current_deviations.append(str(deviation["Description"]))
+                if deviation["Description"] in config.SHORT_TRAIN_MARK:
+                    short_train = True
+        parsed_trains.append(
+            {
+                "northbound": northbound_train,
+                "arrival_date": train["AdvertisedTimeAtLocation"].split('T')[0],
+                "time_delta_seconds": time_delta.total_seconds(),
+                "arrival_time": str(datetime.time(arrival_time)),
+                "end_station": station_names[train["ToLocation"][0]["LocationName"]],
+                "canceled": train["Canceled"],
+                "short_train": short_train,
+                "deviation": current_deviations
+            }
+        )
+        if "Deviation" in train and train["Deviation"][0]["Description"] not in config.UNIMPORTANT_MESSAGES:
+            current_messages.append(str(train["Deviation"][0]["Description"]))
 
-def count_relevant_train_data(parsed_trains, direction):
-    if str(direction).upper() == 'B':
-        return bool(parsed_trains['north'] >= config.RELEVANT_TRAIN_COUNT_NORTH and parsed_trains['south'] >= config.RELEVANT_TRAIN_COUNT_SOUTH)
-    elif str(direction).upper() == 'N':
-        return bool(parsed_trains['north'] >= config.RELEVANT_TRAIN_COUNT_NORTH)
-    elif str(direction).upper() == 'S':
-        return bool(parsed_trains['south'] >= config.RELEVANT_TRAIN_COUNT_SOUTH)
-    else:
-        logging.error(f'Unexpected direction value in count_relevant_train_data: {direction}')
+    with open(config.TRAIN_PARSED_CACHE_FILENAME, 'w') as f:
+        json.dump({
+            "parsed_at": f"{datetime.now()}",
+            "deviations": current_messages,
+            "data": parsed_trains
+        }, f)
 
 if __name__ == "__main__":
     main()
