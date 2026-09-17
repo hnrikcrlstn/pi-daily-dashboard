4c99c0abefba8e7aab7c40bdc5aa9f73d795cbdd
Henrik <henrik.carlstein@gmail.com>
2026-09-17 19:34:08 +0200

    Converted icons to png for rendering with pillow

--- weather.py
+++ weather.py
@@ -18,183 +18,187 @@ def parse_weather_codes(code):
         case 0:
             return {
                 "name": "Molnfritt",
-                "day_icon": "clear-day.svg",
-                "night_icon": "clear-night.svg"
+                "day_icon": "clear-day.png",
+                "night_icon": "clear-night.png"
             }
         case 1:
             return {
                 "name": "Lätt molnigt",
-                "day_icon": "cloudy-1-day.svg",
-                "nigth_icon": "cloudy-1-night.svg"
+                "day_icon": "cloudy-1-day.png",
+                "nigth_icon": "cloudy-1-night.png"
             }
         case 2:
             return {
                 "name": "Molnigt",
-                "day_icon": "cloudy-2-day.svg",
-                "nigth_icon": "cloudy-2-night.svg"
+                "day_icon": "cloudy-2-day.png",
+                "nigth_icon": "cloudy-2-night.png"
             }
         case 3:
             return {
                 "name": "Kraftigt molnigt",
-                "day_icon": "cloudy-3-day.svg",
-                "nigth_icon": "cloudy-3-night.svg"
+                "day_icon": "cloudy-3-day.png",
+                "nigth_icon": "cloudy-3-night.png"
             }
         case 45:
             return {
                 "name": "Dimma",
-                "day_icon": "haze-day.svg",
-                "night_icon": "haze-night.svg"
+                "day_icon": "haze-day.png",
+                "night_icon": "haze-night.png"
             }
         case 48:
             return {
                 "name": "Isdimma",
-                "day_icon": "fog-day.svg",
-                "night_icon": "fog-night.svg"
+                "day_icon": "fog-day.png",
+                "night_icon": "fog-night.png"
             }
         case 51:
             return {
                 "name": "Lätt duggregn",
-                "day_icon": "rainy-1-day.svg",
-                "night_icon": "rainy-1-night.svg"
+                "day_icon": "rainy-1-day.png",
+                "night_icon": "rainy-1-night.png"
             }
         case 53:
             return {
                 "name": "Duggregn",
-                "day_icon": "rain-and-sleet-mix.svg",
-                "night_icon": "rain-and-sleet-mix.svg"
+                "day_icon": "rain-and-sleet-mix.png",
+                "night_icon": "rain-and-sleet-mix.png"
             }
         case 55:
             return {
                 "name": "Kraftigt duggregn",
-                "day_icon": "rainy-1-day.svg",
-                "night_icon": "rainy-1-night.svg"
+                "day_icon": "rainy-1-day.png",
+                "night_icon": "rainy-1-night.png"
             }
         case 56:
             return {
                 "name": "Underkylt regn",
-                "day_icon": "rainy-1-day.svg",
-                "night_icon": "rainy-1-night.svg"
+                "day_icon": "rainy-1-day.png",
+                "night_icon": "rainy-1-night.png"
             }
         case 57:
             return {
                 "name": "Underkylt regn",
-                "day_icon": "rainy-1-day.svg",
-                "night_icon": "rainy-1-night.svg"
+                "day_icon": "rainy-1-day.png",
+                "night_icon": "rainy-1-night.png"
             }
         case 61:
             return {
                 "name": "Lätt regn",
-                "day_icon": "rainy-2-day.svg",
-                "night_icon": "rainy-3-night.svg"
+                "day_icon": "rainy-2-day.png",
+                "night_icon": "rainy-3-night.png"
             }
         case 63:
             return {
                 "name": "Regn",
-                "day_icon": "rain-2-day.svg",
-                "night_icon": "rain-2-night.svg"
+                "day_icon": "rain-2-day.png",
+                "night_icon": "rain-2-night.png"
             }
         case 65:
             return {
                 "name": "Kraftigt regn",
-                "day_icon": "rain-3-day.svg",
-                "night_icon": "rain-3-night.svg"
+                "day_icon": "rain-3-day.png",
+                "night_icon": "rain-3-night.png"
             }
         case 66:
             return {
                 "name": "Snöblandat regn",
-                "day_icon": "snow-and-sleet-mix.svg",
-                "night_icon": "snow-and-sleet-mix.svg"
+                "day_icon": "snow-and-sleet-mix.png",
+                "night_icon": "snow-and-sleet-mix.png"
             }
         case 67:
             return {
                 "name": "Kraftigt snöblandat regn",
-                "day_icon": "rain-and-snow-mix.svg",
-                "night_icon": "rain-and-snow-mix.svg"
+                "day_icon": "rain-and-snow-mix.png",
+                "night_icon": "rain-and-snow-mix.png"
             }
         case 71:
             return {
                 "name": "Lätt snö",
-                "day_icon": "snowy-1-day.svg",
-                "night_icon": "snowy-1-night.svg"
+                "day_icon": "snowy-1-day.png",
+                "night_icon": "snowy-1-night.png"
             }
         case 73:
             return {
                 "name": "Snö",
-                "day_icon": "snowy-2-day.svg",
-                "night_icon": "snowy-2-night.svg"
+                "day_icon": "snowy-2-day.png",
+                "night_icon": "snowy-2-night.png"
             }
         case 75:
             return {
                 "name": "Kraftig snö",
-                "day_icon": "snowy-3-day.svg",
-                "night_icon": "snowy-3-night.svg"
+                "day_icon": "snowy-3-day.png",
+                "night_icon": "snowy-3-night.png"
             }
         case 77:
             return {
                 "name": "Hagel",
-                "day_icon": "hail.svg",
-                "night_icon": "hail.svg"
+                "day_icon": "hail.png",
+                "night_icon": "hail.png"
             }
         case 80:
             return {
                 "name": "Spidda skurar",
-                "day_icon": "rain-1-day.svg",
-                "night_icon": "rain-2-night.svg"
+                "day_icon": "rain-1-day.png",
+                "night_icon": "rain-2-night.png"
             }
         case 81:
             return {
                 "name": "Skurar",
-                "day_icon": "rain-2-day.svg",
-                "night_icon": "rain-2-night.svg"
+                "day_icon": "rain-2-day.png",
+                "night_icon": "rain-2-night.png"
             }
         case 82:
             return {
                 "name": "Kraftiga skurar",
-                "day_icon": "rain-3-day.svg",
-                "night_icon": "rain-3-night.svg"
+                "day_icon": "rain-3-day.png",
+                "night_icon": "rain-3-night.png"
             }
         case 85:
             return {
                 "name": "Snöskurar",
-                "day_icon": "snowy-1-day.svg",
-                "night_icon": "snowy-1-night.svg"
+                "day_icon": "snowy-1-day.png",
+                "night_icon": "snowy-1-night.png"
             }
         case 86:
             return {
                 "name": "Kraftiga snöskurar",
-                "day_icon": "snowy-3-day.svg",
-                "night_icon": "snowy-3-night.svg"
+                "day_icon": "snowy-3-day.png",
+                "night_icon": "snowy-3-night.png"
             }
         case 95:
             return {
                 "name": "Åska",
-                "day_icon": "thunderstorms.svg",
-                "night_icon": "thunderstorms.svg"
+                "day_icon": "thunderstorms.png",
+                "night_icon": "thunderstorms.png"
             }
         case 96:
             return {
                 "name": "Åska med hagel",
-                "day_icon": "thunderstorms.svg",
-                "night_icon": "thunderstorms.svg"
+                "day_icon": "thunderstorms.png",
+                "night_icon": "thunderstorms.png"
             }
         case 99:
             return {
                 "name": "Åska, mycket hagel",
-                "day_icon": "thunderstorms.svg",
-                "night_icon": "thunderstorms.svg"
+                "day_icon": "thunderstorms.png",
+                "night_icon": "thunderstorms.png"
             }
         case None:
             return {
                 "name": "Väderkod hittades inte",
-                "day_icon": "tornado.svg",
-                "night_icon": "tornado.svg"
+                "day_icon": "tornado.png",
+                "night_icon": "tornado.png"
             }
     return None
 
 def main():
-    primary_location_weather = fetch_weather(config.WEATHER_GPS_PRIMARY_LOCATION)
+    primary_location_weather = parse_weather(
+        fetch_weather(config.WEATHER_GPS_PRIMARY_LOCATION)
+    )
     time.sleep(config.API_CURTESY_WAIT_SECONDS)
-    secondary_location_weather = fetch_weather(config.WEATHER_GPS_SECONDARY_LOCATION)
+    secondary_location_weather = parse_weather(
+        fetch_weather(config.WEATHER_GPS_SECONDARY_LOCATION)
+    )
     now = datetime.now()
 
     utils.atomic_write(config.WEATHER_CACHE_FILENAME, {
@@ -217,7 +221,7 @@ def fetch_weather(location):
 def parse_weather(weather):
     if not weather:
         return False
-    
+
     now = datetime.now()
 
     index_3_hours = now.hour + WEATHER_TIME_INDEX_3H_DELAY
